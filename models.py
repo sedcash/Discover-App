1fbfe29160b1f94e7427b599bdd8c8c540fc5e95
Cashaw Jr <sedrick.cashaw.jr@intel.com>
2017-08-15 22:36:40 -0700

    Save a User to database

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    uid = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True)
--- routes.py
+++ routes.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db
 from forms import SignupForm
+from models import User
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sed123@localhost/learning-flask'
 db.init_app(app)
+
 
 app.secret_key = "development-key"
 
@@ -22,6 +24,9 @@ def signup():
         if form.validate() == False:
             return render_template("signup.html", form=form)
         else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
             return "Thank you for signing up and becoming a valuable member"
     elif request.method == "GET":
         return render_template("signup.html", form=form)
